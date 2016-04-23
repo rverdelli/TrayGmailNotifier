b1cce69e6711acc93bce0136c2c3ccdd0e6657f9
rverdelli <roberto.verdelli@techedgegroup.com>
2016-04-23 11:00:09 +0200

    Added todo list Introduced new foundationII version

--- TrayGmailNotifier/TrayGmailNotifier/AppConfig.cs
+++ TrayGmailNotifier/TrayGmailNotifier/AppConfig.cs
@@ -10,14 +10,14 @@ namespace TrayGmailNotifier
     public static class AppConfig
     {
         private static readonly string xmlFileUri = "config.xml";
-        public static Parameter<int> NotificationsFadeInDelay = new XmlPersistentParameter<int>("NotificationsFadeInDelay", 250, xmlFileUri);
-        public static Parameter<int> NotificationsFadeInSteps = new XmlPersistentParameter<int>("NotificationsFadeInSteps", 20, xmlFileUri);
-        public static Parameter<int> NotificationsPersistDelay = new XmlPersistentParameter<int>("NotificationsPersistDelay", 5000, xmlFileUri);
-        public static Parameter<int> DelayBetweenChecks = new XmlPersistentParameter<int>("DelayBetweenChecks", 15000, xmlFileUri);
-        public static Parameter<string> UserName = new XmlPersistentParameter<string>("Username", "", xmlFileUri);
+        public static IParameter<int> NotificationsFadeInDelay = new XmlPersistentParameter<int>("NotificationsFadeInDelay", 250, xmlFileUri);
+        public static IParameter<int> NotificationsFadeInSteps = new XmlPersistentParameter<int>("NotificationsFadeInSteps", 20, xmlFileUri);
+        public static IParameter<int> NotificationsPersistDelay = new XmlPersistentParameter<int>("NotificationsPersistDelay", 5000, xmlFileUri);
+        public static IParameter<int> DelayBetweenChecks = new XmlPersistentParameter<int>("DelayBetweenChecks", 15000, xmlFileUri);
+        public static IParameter<string> UserName = new XmlPersistentParameter<string>("Username", "", xmlFileUri);
 
-        public static Parameter<string> NoNewMailIconPath = new Parameter<string>(@"Icons\NoNewMail.ico");
-        public static Parameter<string> NewMailIconPath = new Parameter<string>(@"Icons\NewMail.ico");
-        public static Parameter<string> NotWorking = new Parameter<string>(@"Icons\NotWorking.ico");
+        public static IParameter<string> NoNewMailIconPath = new Parameter<string>(@"Icons\NoNewMail.ico");
+        public static IParameter<string> NewMailIconPath = new Parameter<string>(@"Icons\NewMail.ico");
+        public static IParameter<string> NotWorking = new Parameter<string>(@"Icons\NotWorking.ico");
     }
 }
