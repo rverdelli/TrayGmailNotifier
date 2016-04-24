ed5938d08570cc83463fc94ae0cfcb5607c3e177
rverdelli <roberto.verdelli@techedgegroup.com>
2016-04-24 23:38:46 +0200

    Added cool code from stackoverflow to show topmost notification without stealing the focus

--- TrayGmailNotifier/TrayGmailNotifier/TrayGmailNotifier.cs
+++ TrayGmailNotifier/TrayGmailNotifier/TrayGmailNotifier.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -170,8 +171,7 @@ namespace TrayGmailNotifier
         {
             showMessageForm = true;
             this.Opacity = 0;
-            this.Show();
-            this.TopMost = true;
+            ShowInactiveTopmost(this);
         }
 
         delegate void hideNotificationFormDelegate();
@@ -306,5 +306,35 @@ namespace TrayGmailNotifier
             System.Environment.Exit(0);
         }
 
+
+        #region ShowInactiveTopmost
+        //http://stackoverflow.com/questions/156046/show-a-form-without-stealing-focus
+
+        private const int SW_SHOWNOACTIVATE = 4;
+        private const int HWND_TOPMOST = -1;
+        private const uint SWP_NOACTIVATE = 0x0010;
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        static extern bool SetWindowPos(
+             int hWnd,             // Window handle
+             int hWndInsertAfter,  // Placement-order handle
+             int X,                // Horizontal position
+             int Y,                // Vertical position
+             int cx,               // Width
+             int cy,               // Height
+             uint uFlags);         // Window positioning flags
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static void ShowInactiveTopmost(Form frm)
+        {
+            ShowWindow(frm.Handle, SW_SHOWNOACTIVATE);
+            SetWindowPos(frm.Handle.ToInt32(), HWND_TOPMOST,
+            frm.Left, frm.Top, frm.Width, frm.Height,
+            SWP_NOACTIVATE);
+        }
+
+        #endregion
     }
 }
